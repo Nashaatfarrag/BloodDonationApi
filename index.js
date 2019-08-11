10b01d6067cb879e9e4e08a88441abe9f291f975
Mohamed Nashaat <mohammed30816894@f-eng.tanta.edu.eg>
2019-08-11 13:58:41 +0200

    fixing Access-control

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-const express = require('express');
-const morgan = require('morgan');
-var bodyParser = require('body-parser')
-const Donor = require('./routes/donor');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const Donor = require("./routes/donor");
 const app = express();
 
+app.use(morgan("tiny"));
 app.use(express.json());
+app.use(cors());
+
 // to make request body as json object
 // app.use(express.urlencoded({ extended: true })); //to understand url parameters
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-    // const sortBy = req.query.sortBy;
-    res.send("hello world");}
-);
+app.get("/", (req, res) => {
+  // const sortBy = req.query.sortBy;
+  res.send("hello world");
+});
 
 app.use("/donor/", Donor);
 
 let port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`listening on port ${port} ... `)
+  console.log(`listening on port ${port} ... `);
 });
