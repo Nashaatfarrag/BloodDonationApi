1cae61c3e5a0943bc616368b265c35e2dee3af5c
Mohamed Nashaat <mohammed30816894@f-eng.tanta.edu.eg>
2019-08-11 14:11:44 +0200

    api access issue

--- index.js
+++ index.js
@@ -4,22 +4,26 @@ const cors = require("cors");
 const Donor = require("./routes/donor");
 const app = express();
 
+var corsOptions = {
+    origin: 'https://gracious-visvesvaraya-237cb1.netlify.com',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 app.use(morgan("tiny"));
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 // to make request body as json object
 // app.use(express.urlencoded({ extended: true })); //to understand url parameters
 app.use(express.static("public"));
 
+app.use("/donor/", Donor);
 app.get("/", (req, res) => {
   // const sortBy = req.query.sortBy;
   res.send("hello world");
 });
 
-app.use("/donor/", Donor);
-
 let port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   console.log(`listening on port ${port} ... `);
 });
