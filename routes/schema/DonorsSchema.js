4474f50842c068fec5a26fc0acf0466f2355ef33
Mohamed Nashaat <mohammed30816894@f-eng.tanta.edu.eg>
2020-10-04 02:31:52 +0200

    update donorschema

--- routes/schema/DonorsSchema.js
+++ routes/schema/DonorsSchema.js
@@ -1,31 +1,34 @@
 const mongoose = require("mongoose");
+let dataBaseURI =
+  "mongodb+srv://brad123:brad123@cluster0.otwbz.gcp.mongodb.net/Blood?retryWrites=true&w=majority";
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/BloodDonation", {
-    useNewUrlParser: true
+  .connect(process.env.MONGODB_URI || dataBaseURI, {
+    useNewUrlParser: true,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log("connected to db successfully"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err.errmsg));
 
 const donorSchema = new mongoose.Schema({
   name: String,
   bloodType: String,
   contactInfo: {
-    tel: { type: String, required: true  },
-    mail: String
+    tel: { type: String, required: true },
+    mail: String,
   },
   imgUrl: String,
   basicInfo: {
-    nationalId: { type : Number ,  required : true , index : true  },
+    nationalId: { type: Number, required: true },
     birthDate: Date,
     gender: String,
-    Address: { type: String, default: "Tatay-Santa-Gharbyia" }
+    Address: { type: String, default: "Tatay-Santa-Gharbyia" },
   },
   donationDates: [
     {
       when: Date,
-      toWhom: String
-    }
-  ]
+      toWhom: String,
+    },
+  ],
 });
 
 const Donor = mongoose.model("DonorTable", donorSchema);
